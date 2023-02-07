e45682011a3246194a088a07f697e84f8beb43b5
Tim Jenness <tjenness@lsst.org>
2023-02-07 14:33:24 -0700

    Remove unneeded lsstimport import

--- python/lsst/__init__.py
+++ python/lsst/__init__.py
@@ -1,10 +1,4 @@
 # See COPYRIGHT file at the top of the source tree.
-
-try:
-    # verify_metrics does not formally use lsstimport, but we still
-    # attempt to import it for compatiblity with the lsst Stack.
-    import lsstimport  # noqa: F401
-except ImportError:
-    pass
 import pkgutil
+
 __path__ = pkgutil.extend_path(__path__, __name__)
